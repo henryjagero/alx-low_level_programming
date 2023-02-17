3ae684c955ef826a3e7eaf2aebe43cf51b1d74a5
henryjagero <nashonnhenry2@gmail.com>
2023-02-18 00:44:52 +0300

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,12 @@
  * Return 0
  */
 int main(void)
-{char;
-int b;
-long int c;
-long long int d;
-float f;
+{  
+       char;
+       int b;
+       long int c;
+       long long int d;
+       float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
