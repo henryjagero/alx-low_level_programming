1485cfed50f0f8467647e22e1eedb69760017c5f
henryjagero <nashonnhenry2@gmail.com>
2023-02-22 11:31:03 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * main - prints the alphabet in lowercase,followed by a new line
+ *
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for(letter = 'a'; letter <= 'z' letter++)
 		_putchar(letter);
 
-	_putchar('\n');
-}
+	_putchar('\n')
