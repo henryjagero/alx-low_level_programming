c3b7c9d493f7d70b25f63cca8554a4125efdd0fc
henryjagero <nashonnhenry2@gmail.com>
2023-03-13 00:52:45 +0300

    0-reset_to_98.c

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
 
-int main () {
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n = 98);
-{
-return 0;
+	*n = 98;
 }
